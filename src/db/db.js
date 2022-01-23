703cd2736b21e4f5a796ac72cbd3c052aee73d93
dhananjay <dhananjay.infimax@gmail.com>
2022-01-23 16:55:17 +0530

    added new database connection

--- src/db/db.js
+++ src/db/db.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb://127.0.0.1:27017/Sample-Project`,{});
+mongoose.connect( "mongodb+srv://Dhananjay:0000@cluster0.ptc5h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{});
+
+//localDatabase
+// mongoose.connect(`mongodb://127.0.0.1:27017/Sample-Project`,{});
